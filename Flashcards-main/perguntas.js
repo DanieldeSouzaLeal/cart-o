78b6669dfeffbb1acafe2ff3e4e854e2ab2ecd48
DanieldeSouzaLeal <134936879+DanieldeSouzaLeal@users.noreply.github.com>
2024-10-10 11:47:59 -0300

    Add files via upload

---
+++ Flashcards-main/perguntas.js
@@ -0,0 +1,162 @@
+criaCartao(
+    'Qual a acor do ',
+    'O que é gravidade?',
+    'De acordo com a relatividade geral, é a deformação do espaço tempo causado por um objeto massivo como uma estrela ou um planeta, por exemplo'
+)
+criaCartao(
+    'Carros',
+    'Qual é o nome do personagem principal em "Carros"?',
+    'O personagem principal em "Carros" é Relâmpago McQueen.'
+)
+
+criaCartao(
+    'Carros',
+    'Quem é o dublador de Relâmpago McQueen em "Carros"?',
+    'O dublador de Relâmpago McQueen é Owen Wilson.'
+)
+
+criaCartao(
+    ' Carros',
+    'Qual é o nome da cidade fictícia onde se passa grande parte do filme "Carros"?',
+    'A cidade fictícia é Radiator Springs.'
+)
+
+criaCartao(
+    ' Carros',
+    'Quem é o rival de Relâmpago McQueen na corrida final de "Carros"?',
+    'O rival de Relâmpago McQueen na corrida final é Chick Hicks.'
+)
+
+criaCartao(
+    ' Carros',
+    'Qual é a profissão de Doc Hudson em "Carros"?',
+    'Doc Hudson é um médico de carros e ex-campeão de corridas.'
+)
+
+criaCartao(
+    'Carros ',
+    'Qual carro é o melhor amigo de Relâmpago McQueen? ',
+    'O melhor amigo de Relâmpago McQueen é Mate.'
+)
+
+criaCartao(
+    'Carros',
+    'Quem é a personagem feminina principal em "Carros"?',
+    'A personagem feminina principal é Sally Carrera.'
+)
+
+criaCartao(
+    ' Carros',
+    'Qual é o nome do caminhão-reboque que ajuda Relâmpago McQueen em "Carros"?',
+    'O caminhão-reboque é chamado Mate.'
+)
+
+criaCartao(
+    'Carros',
+    'Qual é o tema central de "Carros"?',
+    'O tema central de "Carros" é a amizade e o valor de parar e apreciar a vida.'
+)
+
+criaCartao(
+    'Carros',
+    'Qual é a lição que Relâmpago McQueen aprende no final de "Carros"?',
+    'Relâmpago McQueen aprende que ganhar corridas não é tudo e que a amizade e o respeito são mais importantes.'
+)
+criaCartao(
+    ' Carros 2',
+    'Qual é o novo papel de Relâmpago McQueen em "Carros 2"?',
+    'Relâmpago McQueen se torna um participante de um campeonato mundial de corridas em "Carros 2".'
+)
+
+criaCartao(
+    ' Carros 2',
+    'Quem é o novo parceiro de Relâmpago McQueen em "Carros 2"?',
+    'O novo parceiro de Relâmpago McQueen é Mate.'
+)
+
+criaCartao(
+    ' Carros 2',
+    'Qual é o nome da organização secreta em "Carros 2"?',
+    'A organização secreta em "Carros 2" é chamada de "Allinol".'
+)
+
+criaCartao(
+    ' Carros 2',
+    'Qual é o nome do vilão principal em "Carros 2"?',
+    'O vilão principal em "Carros 2" é o Sir Miles Eixoderroda'
+)
+
+criaCartao(
+    ' Carros 2',
+    'Em quais países Relâmpago McQueen e Mate viajam em "Carros 2"?',
+    'Eles viajam para Itália, Japão e Reino Unido.'
+)
+
+criaCartao(
+    ' Carros 2',
+    'Qual é a nova habilidade de Mate em "Carros 2"?',
+    'Mate descobre que é um espião disfarçado com habilidades especiais.'
+)
+
+criaCartao(
+    'Carros 2',
+    'Qual é o novo interesse amoroso de Mate em "Carros 2"?',
+    'O novo interesse amoroso de Mate é a carroça britânica Holly Shiftwell.'
+)
+
+criaCartao(
+    'Carros 2',
+    'Qual é a grande revelação sobre o verdadeiro vilão em "Carros 2"?',
+    'O verdadeiro vilão é Sir Miles Axlerod, que tenta sabotar o campeonato mundial.'
+)
+criaCartao(
+    'Gato de Botas',
+    'Qual é o objetivo principal do Gato de Botas no filme?',
+    'O Gato de Botas busca encontrar a famosa "Fava Mágica" para restaurar sua honra.'
+)
+criaCartao(
+    'Gato de Botas?',
+    'Quem são os principais aliados do Gato de Botas?',
+    'Os principais aliados são Kitty Pata Nela e o gato Humpty Dumpty.'
+)
+criaCartao(
+    'Gato de Botas ',
+    'Qual é a relação entre o Gato de Botas e Humpty Dumpty?',
+    'Humpty Dumpty é um amigo de infância do Gato de Botas, mas eles se afastaram devido a desentendimentos.'
+)
+criaCartao(
+    'Gato de Botas',
+    'Qual é a importância da fada mágica na história?',
+    'A fava mágica é um elemento crucial que pode trazer sorte e poder, centralizando a trama do filme.'
+)
+criaCartao(
+    'Gato de Botas',
+    'Quem é a vilã do filme "Gato de Botas"?',
+    'A vilã é a astuta e poderosa a "Mãe de todos os gatos", que busca controlar a fava mágica.'
+)
+criaCartao(
+    'Gato de Botas ',
+    'Como o Gato de Botas demonstra bravura ao longo do filme?',
+    'Ele enfrenta perigos e se arrisca para proteger seus amigos e alcançar seus objetivos.'
+)
+criaCartao(
+    'Gato de Botas',
+    'Qual é a mensagem central do filme?',
+    'A mensagem central é sobre amizade, lealdade e a importância de confiar em si mesmo.'
+)
+criaCartao(
+    'Gato de Botas',
+    'Como Kitty Pata Nela ajuda o Gato de Botas?',
+    'Ela se torna uma aliada valiosa, usando suas habilidades para superar desafios.'
+)
+criaCartao(
+    'Gato de Botas',
+    'Qual é o papel do humor no filme?',
+    'O humor é essencial para o entretenimento, com muitas cenas engraçadas e personagens cômicos.'
+)
+criaCartao(
+    'Gato de Botas',
+    'Como a história do Gato de Botas se conecta ao universo de Shrek?',
+    'O Gato de Botas é um personagem popular da franquia Shrek, e seu filme expande sua história e aventuras.'
+)
+
